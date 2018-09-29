510d48fb8748257f8298b12f900685a310e30ff8
XixisDev <42183812+XixisDev@users.noreply.github.com>
2018-09-29 17:36:59 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -17,13 +17,32 @@ bot.on('message', message => {
     }
 
     if(message.content === prefix + "help"){
-        var help_embed = new Discord.RichEmbed().setColor("#01FEDC").addField("Commandes du bot :", "   ~help : Affiche la liste des commandes. \n   ~edtg1 : Affiche l'emploi du temps du groupe 1");
+        var help_embed = new Discord.RichEmbed().setColor("#01FEDC").addField("Commandes du bot :", "   ~help : Affiche la liste des commandes. \n   ~edtg1 : Affiche l'emploi du temps du groupe 1. \n   ~edtg2 : Affiche l'emploi du temps du groupe 2. \n   ~atrium : Envoie le lien d'Atrium. \n   ~pronote : Envoie le lien de Pronote. \n   ~cpro : Envoie le lien de CPro.");
         message.channel.sendEmbed(help_embed);
         console.log("Aide envoyée !");
 
     } else if(message.content === prefix + "edtg1"){
 
         message.channel.send("Emploi du temps du groupe 1", {files: ["./edtg1.jpg"]});
+
+    } else if(message.content === prefix + "edgt2"){
+
+        message.channel.send("Emploi du temps du groupe 2");
+
+    } else if(message.content === prefix + "atrium"){
+
+        var atrium_embed = new Discord.RichEmbed().setColor("#FEA101").addField("Lien vers Atrium : ", "https://www.atrium-paca.fr/");
+        message.channel.sendEmbed(atrium_embed);
+
+    } else if(message.content === prefix + "pronote" || message.content === prefix + "ent"){
+
+        var pronote_embed = new Discord.RichEmbed.setColor("#40BF48").addField("Lien vers Pronote : ", "https://0060002v.index-education.net/pronote/");
+        message.channel.sendEmbed(pronote_embed);
+
+    } else if(message.content === prefix + "cpro"){
+
+        var cpro_embed = new Discord.RichEmbed.setColor("#FEFE3D").addField("Lien vers CPro : ", "https://www.cpro-sti.fr/0060002V/");
+        message.channel.sendEmbed(cpro_embed);
 
     }
 });
